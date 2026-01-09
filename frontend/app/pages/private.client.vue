b75f862b7d74857d5fe5cf62dec981bfc1ea61ed
Roman Novytskyi <a24romnovkal@inspedralbes.cat>
2026-01-09 10:51:51 +0100

    fake json data reciever update

--- frontend/app/pages/private.client.vue
+++ frontend/app/pages/private.client.vue
@@ -1,17 +1,20 @@
 <template>
  <h1>Greetings, {{ username }}! This is your personal cabinet.</h1>
  <h2>Document</h2>
- <h3>Part 1</h3>
- <p>Some bs here, so it looked like I was doing something</p>
- <h3>Part 2</h3>
- <p>Some more bs</p>
- <h3>Part 3</h3>
- <p>Some more bs</p>
+ <section v-for="section in document.sections" :key="section.id">
+    <h3>{{ section.title }}</h3>
+    <p>{{ section.content }}</p>
+  </section>
 </template>
 
 <script setup lang="ts">
     const router = useRouter();
-    const { username } = useUser()
+    const { username } = useUser();
+    const document = ref({
+        title: "",
+        sections: [] as { id: number; title: string; content: string }[]
+    });
+
     async function checkEndpoint() {
         const res = await fetch("http://localhost:3000/me", {
             method: "GET",
@@ -29,8 +32,32 @@
         }
     }
 
+    function loadFakeDocument() {
+    document.value = {
+        title: "User Agreement",
+        sections: [
+        {
+            id: 1,
+            title: "Part 1",
+            content: "Some bs here, so it looked like I was doing something"
+        },
+        {
+            id: 2,
+            title: "Part 2",
+            content: "Some more bs"
+        },
+        {
+            id: 3,
+            title: "Part 3",
+            content: "Some more bs"
+        }
+        ]
+    };
+    }
+
     onMounted(async () => {
         await checkEndpoint();
+        loadFakeDocument();
     });
 
 </script>
